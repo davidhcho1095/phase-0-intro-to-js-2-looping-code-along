77817be04e7d575824ad29e70bdebce7d9ee53f8
David Cho <davidhcho1095@gmail.com>
2022-05-08 13:00:22 -0400

    commit tree 8cc2a80789f510af6eb7b6fe1fded098ccf1545f

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const name = ['Guadalupe', 'Ollie', 'Aki'];
+const event = 'surprise';
+
+function writeCards (names, event) {
+    let newArr = [];
+    for (let i = 0; i < names.length; i++) {
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+        return newArr;
+}
+
+function countDown() {
+    let number = 10;
+    while (number >=0) {
+        console.log(number--);
+        debugger;
+    }
+}
+countDown(10);
